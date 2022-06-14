966a2ab011f2cc02fd318680fdbbb75f2b295531
Adrian Kaczmarek <adrian.kaczmarek@web-solutions.eu>
2022-06-14 20:44:31 +0200

    popup with all features

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+const mainButton = document.querySelector("#alertButton");
+const closeSymbol = document.querySelector("#modul_alert-close");
+const alertText = document.querySelector(".modul_alert-message");
+const alertBackground = document.querySelector("#modul_alert-box");
+const resetBtn = document.querySelector(".modul_alert-resetButton");
+
+let counter = 0;
+
+mainButton.addEventListener("click", () => {
+  alertBackground.classList.add("active");
+  alertBackground.classList.remove("display");
+  document.body.classList.add("opacity");
+  counter++;
+  alertText.textContent = `You have clicked ${counter} times to related button`;
+
+  if (counter > 5) {
+    resetBtn.classList.remove("display");
+    resetBtn.addEventListener("click", () => {
+      counter = 0;
+      resetBtn.classList.add("display");
+      alertText.textContent = `You have clicked ${counter} times to related button`;
+    });
+  }
+});
+
+closeSymbol.addEventListener("click", () => {
+  alertBackground.classList.remove("active");
+  alertBackground.classList.add("display");
+});
